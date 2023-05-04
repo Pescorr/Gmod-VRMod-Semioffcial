bf77109a69eb9c24acccf43e132bdda9b43b8f5c
Pescorr <102401058+Pescorr@users.noreply.github.com>
2023-05-05 07:55:58 +0900

    vrmod_dev_excluded_Originalfiles

--- GmodVR/vrmod_vrmod_semioffcialpatch_2780083257/lua/autorun/vrmodsemioffcial_init.lua
+++ GmodVR/vrmod_vrmod_semioffcialpatch_2780083257/lua/autorun/vrmodsemioffcial_init.lua
@@ -4,7 +4,7 @@ local paths = {}
 CreateConVar("vrmod_dev_excluded_files", "vrmod_sample01.lua,vrmod_sample02.lua", FCVAR_ARCHIVE, "Excluded Lua files separated by semicolons")
 
 -- ConVarの値を取得
-local excludedFilesString = GetConVarString("vrmod_semioffcial_excluded_files")
+local excludedFilesString = GetConVarString("vrmod_dev_excluded_files")
 local excludedFiles = {}
 if excludedFilesString ~= "" then
     for file in string.gmatch(excludedFilesString, "([^,]+)") do
@@ -28,3 +28,38 @@ for k,v in ipairs(paths) do
         end
     end
 end
+
+
+
+-- local paths = {}
+
+-- --
+
+-- -- ConVarを作成
+-- CreateConVar("vrmod_dev_included_files", "", FCVAR_ARCHIVE, "Included Lua files separated by semicolons")
+
+-- -- ConVarの値を取得
+-- local includedFilesString = GetConVarString("vrmod_dev_included_files")
+-- local includedFiles = {}
+-- if includedFilesString ~= "" then
+--     for file in string.gmatch(includedFilesString, "([^;]+)") do
+--         includedFiles[file] = true
+--     end
+-- end
+
+-- local _, folders = file.Find("vrmodUnoffcial/*","LUA")
+-- table.sort(folders, function(a,b) return tonumber(a) < tonumber(b) end)
+-- for k,v in ipairs(folders) do
+--     paths[#paths+1] = "vrmodUnoffcial/"..v.."/"
+-- end
+-- paths[#paths+1] = "vrmodUnoffcial/"
+
+-- for k,v in ipairs(paths) do
+--     for k2,v2 in ipairs(file.Find(v.."*","LUA")) do
+--         -- ConVarで指定されたファイルのみ読み込む
+--         if includedFiles[v2] then
+--             AddCSLuaFile(v..v2)
+--             include(v..v2)
+--         end
+--     end
+-- end
