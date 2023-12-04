ceb3c329b054f03e426be25e9b7f7a9aaa73e4ea
Pescorr <102401058+Pescorr@users.noreply.github.com>
2023-12-05 01:52:51 +0900

    2023/12/05

--- lua/vrmodunoffcial/vrmod_viewmodelinfo.lua
+++ lua/vrmodunoffcial/vrmod_viewmodelinfo.lua
@@ -214,91 +214,91 @@ if CLIENT then
 	g_VR.swepOriginalFovs = g_VR.swepOriginalFovs or {}
 	g_VR.lastUpdatedWeapon = ""
 
-	local function ResetViewmodelInfo()
-		g_VR.viewModel = nil
-		g_VR.openHandAngles = g_VR.defaultOpenHandAngles
-		g_VR.closedHandAngles = g_VR.defaultClosedHandAngles
-		g_VR.currentvmi = nil
-		g_VR.viewModelMuzzle = nil
-	end
+	-- local function ResetViewmodelInfo()
+	-- 	g_VR.viewModel = nil
+	-- 	g_VR.openHandAngles = g_VR.defaultOpenHandAngles
+	-- 	g_VR.closedHandAngles = g_VR.defaultClosedHandAngles
+	-- 	g_VR.currentvmi = nil
+	-- 	g_VR.viewModelMuzzle = nil
+	-- end
 
-	function vrmod.UpdateViewmodelInfo(wep,force)
-		if !IsValid(wep) then
-			ResetViewmodelInfo()
-			g_VR.lastUpdatedWeapon = ""
-			return
-		end
+	-- function vrmod.UpdateViewmodelInfo(wep,force)
+	-- 	if !IsValid(wep) then
+	-- 		ResetViewmodelInfo()
+	-- 		g_VR.lastUpdatedWeapon = ""
+	-- 		return
+	-- 	end
 
-		local class = wep:GetClass()
-		if class == g_VR.lastUpdatedWeapon && !force then return end
+	-- 	local class = wep:GetClass()
+	-- 	if class == g_VR.lastUpdatedWeapon && !force then return end
 
-		local vm = wep:GetWeaponViewModel()
-		if vm == "" or vm == "models/weapons/c_arms.mdl" then
-			ResetViewmodelInfo()
-			g_VR.lastUpdatedWeapon = class
-			return
-		end
+	-- 	local vm = wep:GetWeaponViewModel()
+	-- 	if vm == "" or vm == "models/weapons/c_arms.mdl" then
+	-- 		ResetViewmodelInfo()
+	-- 		g_VR.lastUpdatedWeapon = class
+	-- 		return
+	-- 	end
 
-		-----------------------
+	-- 	-----------------------
 		
-		// Drawing with worldmodels
-		/*if vrmod.GetWeaponDrawMode(wep) == VR_WEPDRAWMODE_VIEWMODEL then
-			ResetViewmodelInfo()
+	-- 	// Drawing with worldmodels
+	-- 	/*if vrmod.GetWeaponDrawMode(wep) == VR_WEPDRAWMODE_VIEWMODEL then
+	-- 		ResetViewmodelInfo()
 
-			vrmod.SetRightHandOpenFingerAngles( g_VR.zeroHandAngles )
-			vrmod.SetRightHandClosedFingerAngles( g_VR.zeroHandAngles )
+	-- 		vrmod.SetRightHandOpenFingerAngles( g_VR.zeroHandAngles )
+	-- 		vrmod.SetRightHandClosedFingerAngles( g_VR.zeroHandAngles )
 
-			g_VR.viewModel = wep
-			return
-		end*/
+	-- 		g_VR.viewModel = wep
+	-- 		return
+	-- 	end*/
 		
-		-------------------------
-		local drawWorld = vrmod.GetWeaponDrawMode(wep) != VR_WEPDRAWMODE_VIEWMODEL
-		g_VR.viewModel = !drawWorld && LocalPlayer():GetViewModel() or wep
+	-- 	-------------------------
+	-- 	local drawWorld = vrmod.GetWeaponDrawMode(wep) != VR_WEPDRAWMODE_VIEWMODEL
+	-- 	g_VR.viewModel = !drawWorld && LocalPlayer():GetViewModel() or wep
 
-		if wep.ViewModelFOV then
-			if !g_VR.swepOriginalFovs[class] then
-				g_VR.swepOriginalFovs[class] = wep.ViewModelFOV
-			end
-			wep.ViewModelFOV = GetConVar("fov_desired"):GetFloat()
-		end
+	-- 	if wep.ViewModelFOV then
+	-- 		if !g_VR.swepOriginalFovs[class] then
+	-- 			g_VR.swepOriginalFovs[class] = wep.ViewModelFOV
+	-- 		end
+	-- 		wep.ViewModelFOV = GetConVar("fov_desired"):GetFloat()
+	-- 	end
 
-		local vmi = g_VR.viewModelInfo[class] or {}
-		local model = vmi.modelOverride or g_VR.viewModel:GetModel()
+	-- 	local vmi = g_VR.viewModelInfo[class] or {}
+	-- 	local model = vmi.modelOverride or g_VR.viewModel:GetModel()
 
-		--create offsets if they don't exist
-		if vmi.offsetPos == nil or vmi.offsetAng == nil then
-			vmi.offsetPos, vmi.offsetAng = Vector(), Angle()
+	-- 	--create offsets if they don't exist
+	-- 	if vmi.offsetPos == nil or vmi.offsetAng == nil then
+	-- 		vmi.offsetPos, vmi.offsetAng = Vector(), Angle()
 
-			local cm = ClientsideModel(wep:GetWeaponViewModel())
-			if IsValid(cm) then
-				cm:SetNoDraw(true)
-				cm:SetupBones()
+	-- 		local cm = ClientsideModel(wep:GetWeaponViewModel())
+	-- 		if IsValid(cm) then
+	-- 			cm:SetNoDraw(true)
+	-- 			cm:SetupBones()
 
-				local bone = cm:LookupBone("ValveBiped.Bip01_R_Hand")
-				if bone then
-					local boneMat = cm:GetBoneMatrix(bone)
-					local bonePos, boneAng = boneMat:GetTranslation(), boneMat:GetAngles()
-					boneAng:RotateAroundAxis(boneAng:Forward(),180)
+	-- 			local bone = cm:LookupBone("ValveBiped.Bip01_R_Hand")
+	-- 			if bone then
+	-- 				local boneMat = cm:GetBoneMatrix(bone)
+	-- 				local bonePos, boneAng = boneMat:GetTranslation(), boneMat:GetAngles()
+	-- 				boneAng:RotateAroundAxis(boneAng:Forward(),180)
 
-					vmi.offsetPos, vmi.offsetAng = WorldToLocal(vector_origin,angle_zero,bonePos,boneAng)
-					vmi.offsetPos = vmi.offsetPos + g_VR.viewModelInfo.autoOffsetAddPos
-				end
+	-- 				vmi.offsetPos, vmi.offsetAng = WorldToLocal(vector_origin,angle_zero,bonePos,boneAng)
+	-- 				vmi.offsetPos = vmi.offsetPos + g_VR.viewModelInfo.autoOffsetAddPos
+	-- 			end
 
-				cm:Remove()
-			end
-		end
+	-- 			cm:Remove()
+	-- 		end
+	-- 	end
 
-		--create finger poses
-		vmi.closedHandAngles = vrmod.GetRightHandFingerAnglesFromModel( model )
+	-- 	--create finger poses
+	-- 	vmi.closedHandAngles = vrmod.GetRightHandFingerAnglesFromModel( model )
 
-		// TODO: ArcVR weapons set this manually, but only on deploy so we want to avoid breaking it
-		vrmod.SetRightHandClosedFingerAngles( vmi.closedHandAngles )
-		vrmod.SetRightHandOpenFingerAngles( vmi.closedHandAngles )
+	-- 	// TODO: ArcVR weapons set this manually, but only on deploy so we want to avoid breaking it
+	-- 	vrmod.SetRightHandClosedFingerAngles( vmi.closedHandAngles )
+	-- 	vrmod.SetRightHandOpenFingerAngles( vmi.closedHandAngles )
 
-		g_VR.viewModelInfo[class] = vmi
-		g_VR.currentvmi = vmi
+	-- 	g_VR.viewModelInfo[class] = vmi
+	-- 	g_VR.currentvmi = vmi
 
-		g_VR.lastUpdatedWeapon = class
-	end
+	-- 	g_VR.lastUpdatedWeapon = class
+	-- end
 end
