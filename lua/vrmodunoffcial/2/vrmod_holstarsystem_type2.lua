3786565b367732b9cbba7ab69917930ca03a838b
Pescorr <102401058+Pescorr@users.noreply.github.com>
2025-05-21 20:11:02 +0900

    2025-05-21

--- lua/vrmodunoffcial/2/vrmod_holstarsystem_type2.lua
+++ lua/vrmodunoffcial/2/vrmod_holstarsystem_type2.lua
@@ -21,7 +21,6 @@ function vrholstersystem2()
             CreateClientConVar("vrmod_pouch_size_" .. i, 12, true, FCVAR_ARCHIVE)
             pouch_locked[i] = false
         end
-
         local function InitializeHolsterSystem()
             local convars = vrmod.GetConvars()
             local pouch = {
@@ -32,18 +31,13 @@ function vrholstersystem2()
                 locked = {},
                 enabled = CreateClientConVar("vrmod_pouch_enabled", 1, true, FCVAR_ARCHIVE)
             }
-
-            -- ConVarの初期化
             for i = 1, pouch.slots do
                 CreateClientConVar("vrmod_pouch_weapon_" .. i, "", true, FCVAR_ARCHIVE)
                 CreateClientConVar("vrmod_pouch_size_" .. i, 12, true, FCVAR_ARCHIVE)
                 pouch.locked[i] = false
             end
-
             return pouch
         end
-
-        -- 安全なポジション更新
         local function UpdatePouchPositions(ply, headPos, headAng)
             if not IsValid(ply) then return end
             local chestBone = ply:LookupBone("ValveBiped.Bip01_Spine")
@@ -52,7 +46,6 @@ function vrholstersystem2()
             local chestPos = ply:GetBonePosition(chestBone)
             local hipPos = ply:GetBonePosition(hipBone)
             if not chestPos or not hipPos then return end
-
             return {
                 [1] = headPos + (headAng:Right() * 7),
                 [2] = headPos - (headAng:Right() * 7),
@@ -61,13 +54,9 @@ function vrholstersystem2()
                 [5] = hipPos - (hipAng:Right() * 16)
             }
         end
-
-        -- VRの状態チェック
         local function IsVRReady()
             return g_VR and g_VR.active and g_VR.threePoints
         end
-
-        -- 遅延初期化
         hook.Add(
             "VRMod_Start",
             "HolsterSystem_Init",
@@ -80,11 +69,9 @@ function vrholstersystem2()
                     prev_hand_in_holster_left_status[i] = false
                     prev_hand_in_holster_right_status[i] = false
                 end
-
                 RunConsoleCommand("vrmod_lua_reset_holster2")
             end
         )
-
         for i = 1, pouch_slots do
             cvars.AddChangeCallback(
                 "vrmod_pouch_size_" .. i,
@@ -93,13 +80,11 @@ function vrholstersystem2()
                 end, "vrmod_pouch_size_callback"
             )
         end
-
         for i = 1, pouch_slots do
             pouch_positions[i] = Vector(0, 0, 0)
             pouch_initial_positions[i] = Vector(0, 0, 0)
             pouch_sizes[i] = GetConVar("vrmod_pouch_size_" .. i):GetFloat()
         end
-
         hook.Add(
             "VRMod_Tracking",
             "vrmod_holster_follow_player",
@@ -123,13 +108,12 @@ function vrholstersystem2()
                 pouch_positions[5] = hipPos - (hipAng:Right() * 16)
             end
         )
-
         hook.Add(
             "VRMod_Input",
             "vrutil_hook_pouchinput",
             function(action, pressed)
                 if not g_VR.active then return end
-                if not pouch_enabled:GetBool() then return end -- ホルスター機能が無効の場合は処理を行わない
+                if not pouch_enabled:GetBool() then return end
                 local function storeWeapon(leftHand)
                     for i = 1, pouch_slots do
                         local hand_pos = leftHand and g_VR.tracking.pose_lefthand.pos or g_VR.tracking.pose_righthand.pos
@@ -139,31 +123,25 @@ function vrholstersystem2()
                                 if not pouch_locked[i] then
                                     LocalPlayer():ConCommand("vrmod_pouch_weapon_" .. i .. " " .. activeWeapon:GetClass())
                                 end
-
                                 LocalPlayer():ConCommand("use weapon_vrmod_empty")
-
                                 return
                             end
-
                             local heldEntity = leftHand and g_VR.heldEntityLeft or g_VR.heldEntityRight
                             if IsValid(heldEntity) then
                                 if pouch_locked[i] then return end
                                 LocalPlayer():ConCommand("vrmod_pouch_weapon_" .. i .. " " .. heldEntity:GetClass())
-                                heldEntity:Remove() -- エンティティを消去
+                                heldEntity:Remove()
                                 if leftHand then
                                     g_VR.heldEntityLeft = nil
                                 else
                                     g_VR.heldEntityRight = nil
                                 end
-
                                 return
                             end
-
                             break
                         end
                     end
                 end
-
                 local function equipWeaponOrEntity(leftHand)
                     if not pouch_enabled:GetBool() then return end
                     if not g_VR.active then return end
@@ -180,7 +158,6 @@ function vrholstersystem2()
                                     else
                                         LocalPlayer():ConCommand("vrmod_lefthand " .. (leftHand and "1" or "0"))
                                     end
-
                                     surface.PlaySound(pouch_pickup_sound:GetString())
                                 else
                                     net.Start("vrmod_test_spawn_entity")
@@ -197,31 +174,26 @@ function vrholstersystem2()
                                     end
                                 end
                             end
-
                             break
                         end
                     end
                 end
-
                 if action == "boolean_left_pickup" and not pressed then
                     storeWeapon(true)
                 elseif action == "boolean_right_pickup" and not pressed then
                     storeWeapon(false)
                 end
-
                 if action == "boolean_left_pickup" and pressed then
                     equipWeaponOrEntity(true)
                 elseif action == "boolean_right_pickup" and pressed then
                     equipWeaponOrEntity(false)
                 end
-
                 if action == "boolean_use" and pressed then
                     for i = 1, pouch_slots do
                         if g_VR.tracking.pose_lefthand.pos:DistToSqr(pouch_positions[i]) < (pouch_sizes[i] * pouch_sizes[i]) then
                             pouch_locked[i] = not pouch_locked[i]
                             break
                         end
-
                         if g_VR.tracking.pose_righthand.pos:DistToSqr(pouch_positions[i]) < (pouch_sizes[i] * pouch_sizes[i]) then
                             pouch_locked[i] = not pouch_locked[i]
                             break
@@ -230,12 +202,11 @@ function vrholstersystem2()
                 end
             end
         )
-
         hook.Add(
             "HUDPaint",
             "vrmod_holstarsystem_left_hudpaint",
             function()
-                if not pouch_enabled:GetBool() then return end -- ホルスター機能が無効の場合は処理を行わない
+                if not pouch_enabled:GetBool() then return end
                 if not pouch_visible_hud:GetBool() then return end
                 if not g_VR.active then return end
                 for i = 1, pouch_slots do
@@ -244,29 +215,24 @@ function vrholstersystem2()
                         local text = GetConVar("vrmod_pouch_weapon_" .. i):GetString()
                         if text ~= "" then
                             if pouch_locked[i] then
-                                text = "＊" .. text .. "＊" -- ロックされている場合は文字列の始まりと終わりに「＊」をつける
+                                text = "・・" .. text .. "・・"
                             end
-
                             draw.SimpleText(text, "DermaLarge", ScrW() * 0.05, ScrH() * 0.9, Color(255, 255, 0, 200), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
                         end
-
                         if not (prev_hand_in_holster_left_status[i] == true) then
                             VRMOD_TriggerHaptic("vibration_left", 0, 0.01, 0.01, 0.01)
                         end
-
                         if not IsValid(ply) then break end
                     end
-
                     prev_hand_in_holster_left_status[i] = is_currently_in_holster_left
                 end
             end
         )
-
         hook.Add(
             "HUDPaint",
             "vrmod_holstarsystem_right_hudpaint",
             function()
-                if not pouch_enabled:GetBool() then return end -- ホルスター機能が無効の場合は処理を行わない
+                if not pouch_enabled:GetBool() then return end
                 if not pouch_visible_hud:GetBool() then return end
                 if not g_VR.active then return end
                 for i = 1, pouch_slots do
@@ -275,24 +241,19 @@ function vrholstersystem2()
                         local text = GetConVar("vrmod_pouch_weapon_" .. i):GetString()
                         if text ~= "" then
                             if pouch_locked[i] then
-                                text = "＊" .. text .. "＊" -- ロックされている場合は文字列の始まりと終わりに「＊」をつける
+                                text = "・・" .. text .. "・・"
                             end
-
                             draw.SimpleText(text, "DermaLarge", ScrW() * 0.95, ScrH() * 0.9, Color(255, 255, 0, 200), TEXT_ALIGN_RIGHT, TEXT_ALIGN_CENTER)
                         end
-
                         if not (prev_hand_in_holster_right_status[i] == true) then
                             VRMOD_TriggerHaptic("vibration_right", 0, 0.01, 0.01, 0.01)
                         end
-
                         if not IsValid(ply) then break end
                     end
-
                     prev_hand_in_holster_right_status[i] = is_currently_in_holster_right
                 end
             end
         )
-
         hook.Add(
             "PostDrawTranslucentRenderables",
             "vrmod_holstarsystem_draw",
@@ -318,7 +279,6 @@ function vrholstersystem2()
             end
         )
     end
-
     if SERVER then
         util.AddNetworkString("vrmod_test_spawn_entity")
         net.Receive(
@@ -330,29 +290,22 @@ function vrholstersystem2()
                 local isLeftHand = net.ReadBool()
                 local spawnedEnt = ents.Create(entClass)
                 if not IsValid(spawnedEnt) then return end
-                local function followAndTryPickup()
+                function followAndTryPickup()
                     if not IsValid(spawnedEnt) then return end
+                    spawnedEnt:Spawn()
                     spawnedEnt:SetPos(handPos)
                     spawnedEnt:SetAngles(handAng)
-                    spawnedEnt:Spawn()
-
                     if IsValid(spawnedEnt) then
-                        timer.Simple(
-                            0.20,
-                            function()
-                        pickup(ply, isLeftHand, spawnedEnt:GetPos(), spawnedEnt:GetAngles())
-                        timer.Remove(ply:SteamID() .. "followAndTryPickup")
-                    end
-                        )
+                        -- vrmod_pickup.lua のサーバーサイド pickup 関数を呼び出す
+                        pickup(ply, isLeftHand, handPos, handAng)
+                        timer.Remove(ply:UserID() .. "followAndTryPickup")
                     end
                 end
-
                 timer.Create(ply:UserID() .. "followAndTryPickup", 0.11, 0, followAndTryPickup)
             end
         )
     end
 end
-
 vrholstersystem2()
 concommand.Add(
     "vrmod_lua_reset_holster2",
